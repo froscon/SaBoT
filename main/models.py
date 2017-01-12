125df0e9493e198ac13c677fdfd46edcd60e9136
Martin Lang <Martin.Lang@rwth-aachen.de>
2017-01-12 23:43:19 +0100

    Prepare UI for change of active year
    Show the active year (and possible other years)
    in the big navbar name on the left.

--- main/models.py
+++ main/models.py
@@ -1,6 +1,17 @@
 from django.db import models
+from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
+
+
 
 # Create your models here.
 class ConferenceYear(models.Model):
+	@staticmethod
+	def ensureExists(current):
+		try:
+			current = ConferenceYear.objects.get(year=current)
+		except ConferenceYear.DoesNotExist:
+			cy = ConferenceYear(year=current)
+			cy.save()
+
 	year = models.PositiveIntegerField(editable=False, verbose_name=_("Conference year that exists in our database"))
--- sabot/template_processors.py
+++ sabot/template_processors.py
@@ -1,6 +1,22 @@
 import datetime
 from django.conf import settings
 
+from sabot.multiYear import getActiveYear
+from main.models import ConferenceYear
+
+
+allYears = None
+
+def getAllYears():
+	# Cache this value. If this changes it is ok for me to restart the
+	# django app.
+	global allYears
+	if allYears is None:
+		ConferenceYear.ensureExists(settings.CURRENT_CONFERENCE_YEAR)
+		allYears = [cy.year for cy in ConferenceYear.objects.all()]
+
+	return allYears
+
 def dates_processor(request):
     return {
 		'reg_deadline_booth' : settings.REGISTRATION_DEADLINE,
@@ -13,4 +29,10 @@ def settings_processor(request):
 	return {
 		'RT_TICKET_URL' : settings.RT_TICKET_URL,
 		'SPONSOR_MAIL' : settings.SPONSOR_MAIL
+	}
+
+def active_year_processor(request):
+	return {
+		'active_year' : getActiveYear(request),
+		'all_years' : getAllYears(),
 	}
