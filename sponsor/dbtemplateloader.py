dde1af0bc5032d7d37181febf7d114dfc728d7ec
Martin Lang <Martin.Lang@rwth-aachen.de>
2017-02-04 16:08:44 +0100

    dbtemplates: Adapt to new design in django
    Adapt to new internal django structure for template loaders
    so that templates can again be loaded from the database again.
    
    This should fix issue #4

--- sponsor/dbtemplateloader.py
+++ sponsor/dbtemplateloader.py
@@ -1,13 +1,23 @@
-from django.template import TemplateDoesNotExist
+from django.template import TemplateDoesNotExist, Origin, Template
 from django.template.loaders.base import Loader as BaseLoader
 
 from sponsor.models import SponsorMailTemplate
 
 class DBLoader(BaseLoader):
 	is_usable = True
-	def load_template(self, template_name, template_dirs=None):
+
+	def get_template_sources(self, template_name, template_dirs=None):
+		tmpls = SponsorMailTemplate.objects.filter(templateName__exact=template_name)
+		for tmpl in tmpls:
+			yield Origin(
+				name = "DBLoader::{}".format(template_name),
+				template_name = template_name,
+				loader=self,
+			)
+
+	def get_contents(self, origin):
 		try:
-			tmpl = SponsorMailTemplate.objects.get(templateName__exact=template_name)
-			return (tmpl.template, "DBLoader::{}".format(template_name))
+			tmpl = SponsorMailTemplate.objects.get(templateName__exact=origin.template_name)
+			return tmpl.template
 		except SponsorMailTemplate.DoesNotExist:
-			raise TemplateDoesNotExist(template_name)
+			raise TemplateDoesNotExist(origin)
